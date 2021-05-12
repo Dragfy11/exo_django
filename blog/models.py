d0f3c1c87c4127de9c61ca0ec888122719ca295e
Dragfy11 <dragfy_dark@hotmail.com>
2021-05-12 16:12:53 +0200

    blog

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=400)
+    body = models.TextField()
+    Author = models.CharField(max_length=200)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
